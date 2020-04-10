071972ae81d370f2a36dd3d2d38d1960fcd6cc57
WALL-W-WEB <2935661624@qq.com>
2020-04-10 22:51:53 +0800

    add:itme-list

--- components/item-list/item-list.js
+++ components/item-list/item-list.js
@@ -1,10 +1,29 @@
 // components/item-list/item-list.js
 Component({
+  options:{
+    addGlobalClass:true
+  },
   /**
    * 组件的属性列表
    */
   properties: {
-
+    src:{
+      type:String,
+      value:'/static/image/2.jpg'
+    },
+    time:{
+      type:Boolean,
+      optionalTypes:[String],
+      value:'33:00'
+    },
+    title:{
+      type:String,
+      value:'biao'
+    },
+    sub:{
+      type:String,
+      value:'fu'
+    }
   },
 
   /**
